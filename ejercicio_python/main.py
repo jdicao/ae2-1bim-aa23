3815147507012d55f32a1e950d6aab19f11368e1
jdicao <jeffdicao@gmail.com>
2023-05-06 14:35:58 -0500

    Agregar documentacion en codigo

--- ejercicio_python/main.py
+++ ejercicio_python/main.py
@@ -1,11 +1,14 @@
 from modelo import Universidad, Carrera
-
+#crear objeto universidad con datos mediante constructor
 universidad = Universidad("UTPL", "Loja", "Diana Jimenez", 5)
+#crear objeto carrera con datos mediante constructor
 carrera = Carrera("Tecnología en Transformación Digital de Empresas","UTPL TEC","Tecnólogo en Transformación Digital de Empresas",4)
 print("-----------------------------------------------------------------------------")
 print("Datos de Universidad: \n")
+#presentar datos seteados en el objeto universidad
 print(universidad)
 print("Datos de carrera: \n")
+#presentar datos seteados en el objeto carrera
 print(carrera)
 print("-----------------------------------------------------------------------------")
 
