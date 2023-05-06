a70cf47a265c228fc05b02c5397ffc5ebbd536e5
jdicao <jeffdicao@gmail.com>
2023-05-06 14:30:37 -0500

    Agregar lineas entre presentacion de mensajes

--- ejercicio_python/main.py
+++ ejercicio_python/main.py
@@ -2,7 +2,10 @@ from modelo import Universidad, Carrera
 
 universidad = Universidad("UTPL", "Loja", "Diana Jimenez", 5)
 carrera = Carrera("Tecnología en Transformación Digital de Empresas","UTPL TEC","Tecnólogo en Transformación Digital de Empresas",4)
+print("-----------------------------------------------------------------------------")
+print("Datos de Universidad: \n")
 print(universidad)
 print("Datos de carrera: \n")
 print(carrera)
+print("-----------------------------------------------------------------------------")
 
