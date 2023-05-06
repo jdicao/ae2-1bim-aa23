231736ba99d49e06c8df80e89eb1a1e1fbe8e3dc
jdicao <jeffdicao@gmail.com>
2023-05-06 14:00:23 -0500

    agregado en la segunda rama(nueva) el ejercicio en python

---
+++ ejercicio_python/main.py
@@ -0,0 +1,8 @@
+from modelo import Universidad, Carrera
+
+universidad = Universidad("UTPL", "Loja", "Diana Jimenez", 5)
+carrera = Carrera("Tecnología en Transformación Digital de Empresas","UTPL TEC","Tecnólogo en Transformación Digital de Empresas",4)
+print(universidad)
+print("Datos de carrera: \n")
+print(carrera)
+
